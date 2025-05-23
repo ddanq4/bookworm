811979e253f6c44b3f19b3f2d104968630e5d745
danq <120566535+ddanq4@users.noreply.github.com>
2025-05-23 18:07:28 +0200

    выф

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     presets: [
         ['@babel/preset-env', { targets: { node: 'current' } }],
+        ['next/babel'],
         ['@babel/preset-react', { runtime: 'automatic' }] // 👈 это фикс
     ],
+    plugins: [['module-resolver', { alias: { '@': './src' } }]]
 };
