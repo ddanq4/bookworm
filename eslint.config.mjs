8fe54daf659e7816737f388b2717644f4a67662e
danq <120566535+ddanq4@users.noreply.github.com>
2025-05-23 15:47:57 +0200

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint-define-config';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
+export default defineConfig({
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['next/core-web-vitals', 'eslint:recommended', 'plugin:react/recommended'],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
 });
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
