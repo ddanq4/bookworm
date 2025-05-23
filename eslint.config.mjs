cb043709fba86677ebcbc1f736b7a06f83c018b8
danq <120566535+ddanq4@users.noreply.github.com>
2025-05-23 15:54:49 +0200

    upd3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,12 @@
-import { defineConfig } from 'eslint-define-config';
+import js from '@eslint/js';
+import next from 'eslint-config-next';
 
-export default defineConfig({
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ['@babel/preset-env', '@babel/preset-react'],
+export default [
+  js.configs.recommended,
+  ...next(),
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
     },
   },
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['next/core-web-vitals', 'eslint:recommended', 'plugin:react/recommended'],
-  rules: {
-    'react/react-in-jsx-scope': 'off'
-  }
-});
+];
